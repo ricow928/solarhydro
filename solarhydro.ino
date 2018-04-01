bf43216ff6de1bd4ac51125ec0496c985fc090ad
Rico <rbwalk@gmail.com>
2018-03-31 18:09:40 -0700

    Safety and VOC changes
    - Added max voltage to allow battery to reach, will disable charge if
    higher
    - Added a check every minute during charging to find panel open-circuit
    voltage
    - Added VOC to display
    - Found measured batt_r values

--- solarhydro.ino
+++ solarhydro.ino
@@ -3,6 +3,7 @@
 #include <Adafruit_GFX.h>
 #include <Adafruit_SSD1306.h>
 
+
 #define OLED_RESET 4
 Adafruit_SSD1306 display(OLED_RESET);
 
@@ -38,16 +39,19 @@ static const unsigned char PROGMEM logo16_glcd_bmp[] =
 int panel_pin = 13;             // Pulls down to energize relay
 float charge_upper = 28.8;      // Volts, above which turn off charging if time is satisfied
 float charge_lower = 28.4;      // Volts, below which begin charging for at least time_lim cycles
+float max_v_batt = 34.0;        // Volts, if battery is over this number, disable charging
 bool charging = true;           // Default to enable charging
 int charge_time = 0;            // Counter for number of cycles of charging
-int time_lim = 60;             // Number of cycles to charge after dropping below min (estimate 1000 ms/cycle)
+int time_lim = 60;              // Number of cycles to charge after dropping below min (estimate 1000 ms/cycle)
 int batt_a_pin = A2;            // battery voltage divider measure pin
 int panel_a_pin = A0;           // panel voltage divider measure pin
 
+float panel_voc = 0.0;          // Global to hold panel Voc
+
 // Setup-specific values
 float Vcc = 5.03;               // Volts supplied by Arduino
-float batt_r1 = 9.80;           // kohms, large resistor of battery bridge
-float batt_r2 = 0.981;          // kohms, small resistor of battery bridge
+float batt_r1 = 9.78;           // kohms, large resistor of battery bridge
+float batt_r2 = 0.979;          // kohms, small resistor of battery bridge
 float panel_r1 = 9.75;          // kohms, large resistor of panel bridge
 float panel_r2 = 0.979;         // kohms, small resistor of panel bridge
 
@@ -96,6 +100,7 @@ void setup()
     display.display();
 
     pinMode(panel_pin, OUTPUT);
+
 }
 
 void loop()
@@ -110,9 +115,9 @@ void loop()
       // Apply panel to battery, start timer
       digitalWrite(panel_pin, LOW);
       charging = true;
-      charge_time = 0;
     }
-    if (v_batt > charge_upper && charge_time > time_lim){
+
+    if ((v_batt > charge_upper && charge_time > time_lim) || (v_batt > max_v_batt)){
       // Remove panel from battery, reset timer
       digitalWrite(panel_pin, HIGH);
       charging = false;
@@ -132,6 +137,49 @@ void loop()
       charge_time += 1;
     }
 
+    // Let's see about the open-circuit voltage
+    // if the panel is disconnected, VOC is just the panel voltage as-is
+    if (!charging){
+      panel_voc = v_panel;
+    }
+    // if the panel is connected, don't necessarily want to interrupt charging to measure,
+    // so only do once
+    if (charging && charge_time == 10){
+      // Tell me what you're doing
+      display.clearDisplay();
+      display.setTextSize(1);
+      display.setTextColor(WHITE);
+      display.setCursor(0,0);
+      display.println("Grabbing panel VOC");
+      display.display();
+      // Disconnect panel
+      digitalWrite(panel_pin, HIGH);
+      // Wait two seconds to let voltage recover (may need more, TBD)
+      display.clearDisplay();
+      display.setTextSize(1);
+      display.setTextColor(WHITE);
+      display.setCursor(0,0);
+      display.println("Waiting two sec");
+      display.display();
+      delay(2000);
+      // Get the analog pin measurement
+      display.clearDisplay();
+      display.setTextSize(1);
+      display.setTextColor(WHITE);
+      display.setCursor(0,0);
+      display.println("Measuring VOC");
+      display.display();
+      panel_ain = pin_average(panel_a_pin, 10, 50);
+      // calculate panel voltage
+      panel_voc = voltage(panel_ain, Vcc, panel_r1, panel_r2);
+      // Reconnect panel
+      digitalWrite(panel_pin, LOW);
+    }
+
+    if (charge_time > time_lim){
+      charge_time = 0;
+    }
+
     // Tell me what you know
     display.clearDisplay();
     display.setTextSize(1);
@@ -142,8 +190,10 @@ void loop()
 
     display.print("Vpanel: "); display.print(v_panel,1); display.println("V");
 
-    display.print("Deadband: "); display.print(charge_lower, 1);
-    display.print("-"); display.print(charge_upper, 1); display.println("V");
+    display.print("Panel VOC: "); display.print(panel_voc,1); display.println("V");
+
+    display.print("Deadband: "); display.print(charge_lower,1);
+    display.print("-"); display.print(charge_upper,1); display.println("V");
 
     display.print("Charge: "); display.println(charging);
 
