323bde2bdaf2a163fa8b3f17aad1788b36b5ccc8
Rico <rbwalk@gmail.com>
2018-03-31 19:19:40 -0700

    Remove some unnecessary globals

--- solarhydro.ino
+++ solarhydro.ino
@@ -12,30 +12,6 @@ Adafruit_SSD1306 display(OLED_RESET);
 #define YPOS 1
 #define DELTAY 2
 
-
-#define LOGO16_GLCD_HEIGHT 16 
-#define LOGO16_GLCD_WIDTH  16 
-static const unsigned char PROGMEM logo16_glcd_bmp[] =
-{ B00000000, B11000000,
-  B00000001, B11000000,
-  B00000001, B11000000,
-  B00000011, B11100000,
-  B11110011, B11100000,
-  B11111110, B11111000,
-  B01111110, B11111111,
-  B00110011, B10011111,
-  B00011111, B11111100,
-  B00001101, B01110000,
-  B00011011, B10100000,
-  B00111111, B11100000,
-  B00111111, B11110000,
-  B01111100, B11110000,
-  B01110000, B01110000,
-  B00000000, B00110000 };
-
-// number of analog samples to take per reading
-#define NUM_SAMPLES 10
-
 int panel_pin = 13;             // Pulls down to energize relay
 float charge_upper = 28.8;      // Volts, above which turn off charging if time is satisfied
 float charge_lower = 28.4;      // Volts, below which begin charging for at least time_lim cycles
